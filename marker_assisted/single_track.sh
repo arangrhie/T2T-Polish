322abf1c7b2dceb32a41b645d8c7787ce2321491
Arang Rhie <arangrhie@users.noreply.github.com>
2020-09-19 10:51:18 -0400

    Update single_track.sh

--- marker_assisted/single_track.sh
+++ marker_assisted/single_track.sh
@@ -4,5 +4,5 @@
 
 asm=$1
 samtools faidx $asm.fasta
-meryl-lookup -dump -memory 8g -sequence $asm.fasta -mers single.meryl | awk '$4=="T" {print $1"\t"$3"\t($3+21)"}' > $asm.single.bed
+meryl-lookup -dump -memory 8g -sequence $asm.fasta -mers single.meryl | awk '$4=="T" {print $1"\t"$3"\t"($3+21)}' > $asm.single.bed
 igvtools count $asm.single.bed $asm.single.tdf $asm.fasta.fai
