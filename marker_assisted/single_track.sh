893bf8be9689b6054ea69c141dd8ebfeab6eae38
Arang Rhie <arangrhie@users.noreply.github.com>
2020-09-19 10:50:00 -0400

    create single.tdf track

---
+++ marker_assisted/single_track.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Requires samtools, meryl-1.0
+
+asm=$1
+samtools faidx $asm.fasta
+meryl-lookup -dump -memory 8g -sequence $asm.fasta -mers single.meryl | awk '$4=="T" {print $1"\t"$3"\t($3+21)"}' > $asm.single.bed
+igvtools count $asm.single.bed $asm.single.tdf $asm.fasta.fai
