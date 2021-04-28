24f159d96c7c977721ccab55c16704aceb5f28b2
arangrhie <arrhie@gmail.com>
2021-04-28 09:30:32 -0400

    simplify sam2paf

--- coverage/sam2paf.sh
+++ coverage/sam2paf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Usage: ~/codes/sam2paf.sh in.bam out.paf"
+echo "Usage: ./sam2paf.sh in.bam out.paf"
 
 if [[ "$#" -lt 2 ]]; then
   echo "No input and output file provied."
@@ -18,10 +18,12 @@ module load minimap2/2.17
 
 if [[ $1 =~ .bam$ ]] || [[ $1 =~ .cram$ ]]; then
   echo "
-  samtools view -h -@$cpu $1 | $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf - | awk \'{print \$1 .. \$16}\' - > $2"
-  samtools view -h -@$cpu $1 | $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf - | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14"\t"$15"\t"$16}' - > $2
+  samtools view -h -@$cpu $1 | $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf - | cut -f1-16 - > $2"
+  samtools view -h -@$cpu $1 | $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf - | cut -f1-16 - > $2
 else
+  ## Add -L for long cs tag form
   echo "
-  $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf $1 | awk \'{print \$1 .. \$16}\' - > $2"
-  $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf $1 | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14"\t"$15"\t"$16}' - > $2
+  $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf $1 | cut -f1-16 - > $2"
+  $tools/k8/k8 /usr/local/apps/minimap2/2.17/misc/paftools.js sam2paf $1 | cut -f1-16 - > $2
+
 fi
