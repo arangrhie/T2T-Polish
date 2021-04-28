4195c745700135b0a533ceb60a44226ddb880437
arangrhie <arrhie@gmail.com>
2021-04-28 10:05:02 -0400

    aggregate the per-sequence cram files and merge

---
+++ marker_assisted/aggregate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ "$#" -lt 1 ]]; then
+	echo "Usage: ./aggregate.sh out-prefix"
+	exit -1
+fi
+
+cpus=$SLURM_CPUS_PER_TASK
+out=$1
+
+aggregate() {
+	prefix=$1
+	out=$2
+	ls *.$prefix.cram > $prefix.list
+
+	echo "
+	Samtools Merge"
+	samtools merge -b $prefix.list -O bam -@$cpus $out.$prefix.bam
+	samtools index $out.$prefix.bam
+
+	echo "
+	Convert to paf"
+	$tools/T2T-Polish/coverage/sam2paf.sh $out.$prefix.bam $out.$prefix.paf
+	java -jar -Xmx4g $tools/T2T-Polish/paf_util/pafToCovWig.jar $out.$prefix.paf "$out" 1024 > $out.$prefix.cov.wig
+}
+
+aggregate markersandlength $asm $out
+aggregate markers $asm $out
+
