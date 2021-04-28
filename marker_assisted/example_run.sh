a91aef9e8266c93ec803802dffc738720c91503e
arangrhie <arrhie@gmail.com>
2021-04-28 10:09:52 -0400

    correct the example run

--- marker_assisted/example_run.sh
+++ marker_assisted/example_run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# ./single_copy_filter.sh clr_test.bam chr20 t2t-chm13.20200602.fasta 20200602.single.meryl
-
 # Submit to cluster
-./_submit.sh clr.bam chr20 t2t-chm13.20200602.fasta 20200602.single.meryl 1
+for i in $(seq 1 22) X
+do
+  ./_submit.sh clr.bam chr$i t2t-chm13.20200602.fasta 20200602.single.meryl 1
+done
+
+# or run init.sh directly
+./init.sh clr.bam chr20 t2t-chm13.20200602.fasta 20200602.single.meryl 1
