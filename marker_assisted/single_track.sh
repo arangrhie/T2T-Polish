2b9d37c44f295fc9435818d8e7f1a6df6b6d076e
arangrhie <arrhie@gmail.com>
2021-09-14 09:42:46 -0400

    comment off counting

--- marker_assisted/single_track.sh
+++ marker_assisted/single_track.sh
@@ -12,14 +12,14 @@ asm=`echo $asm | sed 's/.fasta$//g' | sed 's/.fa$//g'`
 mer=$2
 k=$3
 
-meryl count k=$k $1 output $asm.meryl
-meryl equal-to 1 $asm.meryl output $asm.1.meryl
-meryl intersect $asm.1.meryl $mer output $asm.single.meryl
-meryl-lookup -wig-depth -sequence $1 -mers $asm.single.meryl > $asm.single.k$k.wig
-meryl-lookup -bed -sequence $1 -mers $asm.single.meryl > $asm.single.k$k.bed
+#meryl count k=$k $1 output $asm.k$k.meryl
+#meryl equal-to 1 $asm.k$k.meryl output $asm.k$k.1.meryl
+#meryl intersect $asm.k$k.1.meryl $mer output $asm.k$k.single.meryl
+meryl-lookup -bed -sequence $1 -mers $asm.k$k.single.meryl > $asm.single.k$k.bed
+meryl-lookup -wig-depth -sequence $1 -mers $asm.k$k.single.meryl > $asm.single.k$k.wig
 
-if [[ -z $1.fai ]]; then
+if ! [[ -s $1.fai ]]; then
   samtools faidx
 fi
-igvtools count $asm.single.bed $asm.single.tdf $1.fai
+igvtools count $asm.single.k$k.bed $asm.k$k.single.tdf $1.fai
 
