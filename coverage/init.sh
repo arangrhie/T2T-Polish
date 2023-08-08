1a5a0d048782a33165846294b3423e7cccd23685
arangrhie <arrhie@gmail.com>
2023-08-08 01:31:03 -0400

    use seqtk for telo.bed

--- coverage/init.sh
+++ coverage/init.sh
@@ -3,23 +3,22 @@
 if [[ -z $1 ]] || [[ -z $2 ]]; then
     echo "Usage: ./init.sh <asm.fasta> <name>"
     echo "  <asm.fasta>: absolute path."
-    echo "  <name>: <asm.fasta> will be linked as <name.fasta>"
+    echo "  <name>: <asm.fasta> will be linked as <name.fa>"
     exit 0
 fi
 
 asm=$1
 name=$2
 
-if [ ! -e $name.fasta ]; then
-    echo "Symlink $asm to $name.fasta"
-    ln -s $asm $name.fasta
-fi
+module load seqtk
 
-if [ ! -e $name.fasta.fai ]; then
-    ln -s $asm.fai $name.fasta.fai
+if [[ ! -s $asm.fai ]]; then
+  echo "No $asm.fai found. Exit."
+  exit -1
 fi
+echo "# Get sizes"
+awk '{print $1"\t0\t"$2}' $asm.fai > $name.bed
 
-echo "# Get scaffold ends (1kb)"
-awk '{print $1"\t0\t"$2}' $name.fasta.fai > $name.bed
-cat $name.bed | awk '($3-$2) > 2000 {print $1"\t0\t1000\n"$1"\t"($3-1000)"\t"$3}' > $name.telo.bed  # Ignore scaffolds <2kb
+echo "# Telomere"
+seqtk telo $asm > $name.telo.bed
 
