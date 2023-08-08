1e8e0b596050de34711d225ff7224417c2f58aca
arangrhie <arrhie@gmail.com>
2023-08-08 01:32:06 -0400

    try symlinking path from one dir above

--- coverage/issues.sh
+++ coverage/issues.sh
@@ -19,6 +19,15 @@ platform=$4
 set -x
 set -o pipefail
 
+if [[ ! -s $ver.bed ]]; then
+  echo "Linking files in ../"
+  [[ -s ../$ver.bed ]] && ln -s ../$ver.bed
+  [[ -s ../$ver.error.bed ]] && ln -s ../$ver.error.bed
+  [[ -s ../$ver.exclude.bed ]] && ln -s ../$ver.exclude.bed
+  [[ -s ../$ver.telo.bed ]] && ln -s ../$ver.telo.bed
+  [[ -e ../pattern ]] && ln -s ../pattern
+fi
+
 # Collect wig files
 $tools/T2T-Polish/coverage/collect_summary.sh $in_paf "$name"
 
