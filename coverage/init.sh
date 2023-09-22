251a18ea739feaf59469b8c8a7d2fddb4311b942
arangrhie <arrhie@gmail.com>
2023-09-22 11:21:57 -0400

    move required files to pattern

--- coverage/init.sh
+++ coverage/init.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if [[ -z $1 ]] || [[ -z $2 ]]; then
-    echo "Usage: ./init.sh <asm.fasta> <name>"
-    echo "  <asm.fasta>: absolute path."
-    echo "  <name>: <asm.fasta> will be linked as <name.fa>"
+if [[ -z $1 ]] ; then
+    echo "Usage: ./init.sh <asm.fasta>"
+    echo "  <asm.fasta>: absolute path to asm.fasta,"
+    echo "               expecting to have asm.fasta.fai in the same path"
     exit 0
 fi
 
 asm=$1
-name=$2
+name=`echo $asm | sed 's/\.gz$//g' | sed 's/\.fa$//g' | sed 's/\.fasta$//g'`
+
+mkdir -p pattern && cd pattern
+
+if [[ "$asm" == "*\.gz" ]]; then
+  pigz -dc $asm > $name.fa
+else
+  ln -sf $asm     $name.fa
+  ln -sf $asm.fai $name.fa.fai
+fi
 
 module load seqtk
 
@@ -16,9 +25,15 @@ if [[ ! -s $asm.fai ]]; then
   echo "No $asm.fai found. Exit."
   exit -1
 fi
+
 echo "# Get sizes"
 awk '{print $1"\t0\t"$2}' $asm.fai > $name.bed
 
 echo "# Telomere"
 seqtk telo $asm > $name.telo.bed
 
+echo "# Exclude"
+seqtk gap -l0 $asm > $name.exclude.bed
+
+echo "# Pattern"
+$tools/T2T-Polish/pattern/microsatellites.sh $name.fa
--- coverage/issues.sh
+++ coverage/issues.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [[ "$#" -lt 4 ]]; then
-  echo "Usage: ./issues.sh in.paf name ver platform"
-  echo "  in.paf: input paf file"
-  echo "  name:   name to appear on the .wig files"
-  echo "  ver:    assembly version"
-  echo "  platform: HiFi or ONT"
+if [[ "$#" -lt 5 ]]; then
+  echo "Usage: ./issues.sh in.paf name ver platform pattern"
+  echo "  in.paf   input paf file"
+  echo "  name     name to appear on the .wig files"
+  echo "  ver      assembly version"
+  echo "  platform HiFi, ONT or Hybrid"
+  echo "  pattern  path to pattern folder"
   echo
-  echo "Required: pattern/ver/, ver.bed, ver.error.bed, ver.exclude.bed, ver.telo.bed"
+  echo "Required: pattern/ver/, pattern/ver.bed, pattern/ver.error.bed, pattern/ver.exclude.bed, pattern/ver.telo.bed"
   exit -1
 fi
 in_paf=$1
@@ -15,25 +16,19 @@ name=$2
 in=${in_paf/.paf/}
 ver=$3
 platform=$4
+pattern=$5
+
+ln -sf $pattern pattern
 
 set -x
 set -o pipefail
-
-if [[ ! -s $ver.bed ]]; then
-  echo "Linking files in ../"
-  [[ -s ../$ver.bed ]] && ln -s ../$ver.bed
-  [[ -s ../$ver.error.bed ]] && ln -s ../$ver.error.bed
-  [[ -s ../$ver.exclude.bed ]] && ln -s ../$ver.exclude.bed
-  [[ -s ../$ver.telo.bed ]] && ln -s ../$ver.telo.bed
-  [[ -e ../pattern ]] && ln -s ../pattern
-fi
 
 # Collect wig files
 $tools/T2T-Polish/coverage/collect_summary.sh $in_paf "$name"
 
 # Collect coverage statistics
-$tools/T2T-Polish/coverage/coverage_stat.sh $in.cov.wig
+$tools/T2T-Polish/coverage/coverage_stat.sh $in.cov.wig pattern/$ver.exclude.bed
 
 # Collect low supportive regions
-$tools/T2T-Polish/coverage/low_support.sh $in_paf $ver $platform
+$tools/T2T-Polish/coverage/low_support.sh $in_paf $ver $platform $pattern
 
