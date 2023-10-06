52bd4369c3a708f6b0c268c716a5d39828ebca96
arangrhie <arrhie@gmail.com>
2023-10-06 11:10:04 -0400

    rename mrg hybrid

--- deepvariant/_submit_mrg_hybrid_dv.sh
+++ deepvariant/_submit_mrg_hybrid_dv.sh
@@ -1,13 +1,14 @@
-#! /bin/bash
+#! /bin/sh
 
-if [[ "$#" -lt 5 ]]; then
-  echo "Usage: ./_submit_mrg_dv.sh ref.fasta hifi.bam ilmn.bam out sample [line_num]"
+if [[ "$#" -lt 6 ]]; then
+  echo "Usage: ./_submit_mrg_dv.sh ref.fasta hifi.bam ilmn.bam out sample mq"
   echo "  ref.fasta  reference"
   echo "  hifi.bam   HiFi primary read alignment"
   echo "  ilmn.bam   Illumina read alignment. Use markdup.bam"
   echo "  out        output prefix for the merged bam"
   echo "  sample     sample name to appear in the final VCF"
-  echo "  line_num   line_num to launch the mapping OPTIONAL"
+  echo "  mq         minimum mapping quality requirements. Use positive values for MQ filters. Use -1 to follow pre-set options in DV."
+  echo "             e.g. 0 for all-to-dip, -1 for all-to-hap alignments."
   echo
   echo "  output VCF file will be generated as dv_mode.vcf.gz"
   echo
@@ -20,16 +21,16 @@ hifi_bam=$2
 ilmn_bam=$3
 out=$4
 sample=$5
-mode="HYBRID_PACBIO_ILLUMINA"
+mq=$6
 
-PIPELINE=$tools/T2T-Polish
+mode="HYBRID_PACBIO_ILLUMINA"
 
 path=$PWD
 cpus=12
 mem=24g
 partition=norm
 walltime=12:00:00
-script=$PIPELINE/deepvariant/merge_hybrid.sh
+script=$tools/T2T-Polish/deepvariant/merge_hybrid.sh
 args="$hifi_bam $ilmn_bam $out.bam"
 name=mrg.$out
 log=logs/$name.%A.log
@@ -46,16 +47,14 @@ sbatch -J $name --cpus-per-task=$cpus --mem=$mem  \
        $extra --time=$walltime \
        --error=$log --output=$log $script $args > mrg.jid
 set +x
-
-wait_for=`tail -n1 mrg.jid`
 fi
 
 name=sam2paf.$out
 log=logs/$name.%A.log
-script=$PIPELINE/coverage/sam2paf.sh
+script=$tools/T2T-Polish/coverage/sam2paf.sh
 args="$out.bam $out.pri.paf"
 
-jid=`tail -n1 filt.jid`
+wait_for=`tail -n1 mrg.jid`
 extra="--dependency=afterok:$wait_for"
 
 set -x
@@ -65,5 +64,9 @@ sbatch -J $name --cpus-per-task=$cpus --mem=$mem \
        --error=$log --output=$log $script $args
 set +x
 
-sh $PIPELINE/deepvariant/_submit_deepvariant.sh $ref $out.bam $mode $sample $wait_for
+if [[ $mq -eq -1 ]]; then
+  sh $tools/T2T-Polish/deepvariant/_submit_deepvariant.sh $ref $out.bam $mode $sample $wait_for
+else
+  sh $tools/T2T-Polish/deepvariant/_submit_deepvariant_with_minqual.sh $ref $out.bam $mode $sample $mq $wait_for
+fi
 
