b5fda6f4f17e4b73afad678305a5a00174a06785
arangrhie <arrhie@gmail.com>
2023-10-06 11:04:37 -0400

    fix array task id and region params

--- deepvariant/ont_r9_pepper_margin_dv.sh
+++ deepvariant/ont_r9_pepper_margin_dv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ "$#" -lt 2 ]]; then
+if [[ "$#" -lt 3 ]]; then
   echo "Usage: ./ont_r9_pepper_margin_dv.sh bam ref mq [region]"
   echo "  bam     input bam"
   echo "  ref     reference fasta. REQUIRES .fai file in the same place"
@@ -15,13 +15,24 @@ MQ=$3
 
 THREADS=$SLURM_CPUS_PER_TASK
 
-i=$SLURM_ARRAY_JOB_ID
-REGION=`sed -n ${i}p $REF.fai | cut -f1`
-if [[ "$REGION" == "" ]]; then
-  REGION=$4
+i=$SLURM_ARRAY_TASK_ID
+REGION=$4
+
+if [[ -z $i ]] && [[ -z $REGION ]]; then
+  echo "\$SLURM_ARRAY_TASK_ID=$SLURM_TASK_JOB_ID, no [region] provided. Exit."
+  exit -1
+elif [[ -z $REGION ]]; then
+  REGION=`sed -n ${i}p $REF.fai | cut -f1`
 fi
 
-OUTPUT_DIR=dv_ONT_R9_MQ${MQ}_$REGION
+
+if [[ "$MQ" -eq -1 ]]; then
+  MQ_OPT=""
+  OUTPUT_DIR=dv_ONT_R9_$REGION
+else
+  MQ_OPT="--pepper_min_mapq $MQ --dv_min_mapping_quality $MQ"
+  OUTPUT_DIR=dv_ONT_R9_MQ${MQ}_$REGION
+fi
 
 ulimit -u 8192
 
@@ -34,6 +45,6 @@ set -x
 
 run_pepper_margin_deepvariant call_variant \
   -b $BAM -f $REF -o $OUTPUT_DIR \
-  --pepper_min_mapq $MQ --dv_min_mapping_quality $MQ \
+  $MQ_OPT \
   -t $THREADS -r $REGION --ont_r9_guppy5_sup --gpu
 
