1b6f238aa5c095873526e519dc7a7b8bc7f40bd1
arangrhie <arrhie@gmail.com>
2024-05-15 17:24:07 -0400

    clean up submission scripts for bwa

--- bwa/_submit_bwa.sh
+++ bwa/_submit_bwa.sh
@@ -13,6 +13,9 @@ line_num=$4
 mkdir -p logs
 PIPELINE=$tools/T2T-Polish
 
+set -e
+set -o pipefail
+
 if [[ ! -s $ref.bwt ]]; then
 	cpus=4
 	mem=10g
@@ -24,15 +27,29 @@ if [[ ! -s $ref.bwt ]]; then
 	path=`pwd`
 	log=logs/$name.%A.log
 
-	echo "\
-	sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
+  set -x
 	sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > index.jid
+  set +x
 	index=`cat index.jid`
-	echo $index
+	echo "Indexing: $index"
 	extra="--dependency=$index"
 fi
 
-if [[ ! -s bam.list ]]; then
+LEN=`wc -l $fastq_map | awk '{print $1}'`
+arr=""
+for i in $(seq 1 $LEN)
+do
+  if [[ ! -s $out.$i.dedup.bam ]]; then
+    arr="${arr}${i}_"
+  fi
+done
+
+if [[ -v "$arr" ]]; then
+  echo "Found all *.dedup.bam. Skip mapping"
+  extra=""
+  wait_for=""
+else
+  arr=`echo $arr | sed 's/_/,/g'`
   cpus=24
   mem=120g
   name=map.$out
@@ -43,27 +60,26 @@ if [[ ! -s bam.list ]]; then
   path=`pwd`
   log=logs/$name.%A_%a.log
 
-  LEN=`wc -l $fastq_map | awk '{print $1}'`
   extra="--gres=lscratch:900 $extra"
   if [[ -z $line_num ]]; then
-    extra="$extra --array=1-$LEN"
+    extra="$extra --array=$arr"
   else
     extra="$extra --array=$line_num"
   fi
 
-  echo "\
-  sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
+  # map
+  set -x
   sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > bwa.jid
-  wait_for="--dependency=afterok:"`cat bwa.jid`
+  set +x
+  jid=`cat bwa.jid`
+  echo "Map: $jid"
+  wait_for="--dependency=afterok:$jid"
 
   for i in $(seq 1 $LEN)
   do
     echo "$out.$i.bam" >> bam.list
     echo "$out.$i.dedup.bam" >> bam.dedup.list
   done
-
-else
-  wait_for=""
 fi
 
 cpus=24
@@ -78,14 +94,14 @@ extra=$wait_for
 
 args="$out bam.list"
 
-echo "\
-sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
+set -x
 sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args
+set +x
 
 name=mrg.$out.dedup
 args="$out.dedup bam.dedup.list"
-echo "
-sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
-sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' >> mrg.jid
-
-
+set -x
+sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > mrg.jid
+set +x
+jid=`cat mrg.jid`
+echo "Merge: $jid"
